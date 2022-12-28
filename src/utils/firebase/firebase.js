53898d68f7cc06f2780b7d58192d7caf28fb9763
Hao Chen <hche8093@uni.sydney.edu.au>
2022-12-28 18:27:59 +1100

    Add users to the firestore

--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -5,6 +5,7 @@ import {
 	GoogleAuthProvider,
 	signInWithRedirect,
 } from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
 	apiKey: process.env.REACT_APP_FIREBASE_KEY,
@@ -26,3 +27,32 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+	const userDocRef = doc(db, "users", userAuth.uid);
+
+	console.log(userDocRef);
+
+	const userSnapshot = await getDoc(userDocRef);
+	console.log(userSnapshot);
+	console.log(userSnapshot.exists());
+
+	if (!userSnapshot.exists()) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+
+		try {
+			await setDoc(userDocRef, {
+				displayName,
+				email,
+				createdAt,
+			});
+		} catch (err) {
+			console.log("error creating user", err.message);
+		}
+	}
+
+	return userDocRef;
+};
