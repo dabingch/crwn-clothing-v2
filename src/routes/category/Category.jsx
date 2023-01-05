343c8dd8d1a88548eb69e611c7cf5218bc2cd6d9
Hao Chen <hche8093@uni.sydney.edu.au>
2023-01-05 17:08:16 +1100

    Add and get data from firestore

---
+++ src/routes/category/Category.jsx
@@ -0,0 +1,30 @@
+import './category.styles.scss'
+
+import ProductCard from '../../components/product-card/ProductCard'
+
+import { useContext, useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+
+import { CategoriesContext } from '../../contexts/CategoriesContext'
+
+const Category = () => {
+	const { category } = useParams()
+	const { categoriesMap } = useContext(CategoriesContext)
+
+	const [products, setProducts] = useState(categoriesMap[category])
+
+	useEffect(() => {
+		setProducts(categoriesMap[category])
+	}, [category, categoriesMap])
+
+	return (
+		<div className='category-container'>
+			{products &&
+				products.map((product) => (
+					<ProductCard key={product.id} product={product} />
+				))}
+		</div>
+	)
+}
+
+export default Category
