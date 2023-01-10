eb7798b283e7f90f64523a7a1fd0cccb91f93e95
Hao Chen <hche8093@uni.sydney.edu.au>
2023-01-10 16:43:21 +1100

    Add redux store configuration

--- src/index.js
+++ src/index.js
@@ -7,19 +7,24 @@ import { UserProvider } from './contexts/UserContext'
 import { CategoriesProvider } from './contexts/CategoriesContext'
 import { CartProvider } from './contexts/CartContext'
 
+import { Provider } from 'react-redux'
+import { store } from './store/store'
+
 import './index.scss'
 
 ReactDOM.render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<UserProvider>
-				<CategoriesProvider>
-					<CartProvider>
-						<App />
-					</CartProvider>
-				</CategoriesProvider>
-			</UserProvider>
-		</BrowserRouter>
+		<Provider store={store}>
+			<BrowserRouter>
+				<UserProvider>
+					<CategoriesProvider>
+						<CartProvider>
+							<App />
+						</CartProvider>
+					</CategoriesProvider>
+				</UserProvider>
+			</BrowserRouter>
+		</Provider>
 	</React.StrictMode>,
 	document.getElementById('root')
 )
--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,11 @@
 import { compose, createStore, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
 
+import { rootReducer } from './root-reducer'
+
+// Configure middleware
+const middleWares = [logger]
+const composedEnhancers = compose(applyMiddleware(...middleWares))
+
 // root-reducer
+export const store = createStore(rootReducer, undefined, middleWares)
